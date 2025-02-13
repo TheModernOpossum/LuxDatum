7aa48b8595beb547cb7ddad05fc339d460520d6e
Daniel <146178221+IthNaddaLithp@users.noreply.github.com>
2025-02-12 18:03:24 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,10 +11,10 @@ const MapComponent = dynamic(() => import("../components/MapComponent"), {
 export default function Home() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white p-6">
-            <h1 className="text-4xl font-bold mb-6">LuxDatum Earth Imagery</h1>
+            <h1 className="text-4xl font-bold mb-6">Lux~Datum : NASA Satellite Imagery</h1>
 
             {/* Map - Loaded on Client Only */}
-            <Suspense fallback={<p>Loading map...</p>}>
+            <Suspense fallback={<p>Hang in there, we're working on it...</p>}>
                 <MapComponent />
             </Suspense>
         </div>
